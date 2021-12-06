20406887a43ec4b455ad84c60b42bf0daa47f79d
tetianarozz <tatianarozputnia@gmail.com>
2021-12-06 19:11:02 +0200

    second commit

--- class/person/person.rb
+++ class/person/person.rb
@@ -1,10 +1,24 @@
 class Person
-  def initialize(person_name, person_second_name)
+  def initialize(person_name, person_second_name, person_age)
     @name = person_name
     @second_name = person_second_name
+    @age = person_age
   end
 
   def full_name
-    return "#{@name} #{@second_name}"
+    if @age > 60
+      puts "#{@name} #{@second_name}"
+    else
+      puts "#{@name}"
+
+    end
+  end
+
+  def age_check
+    if @age > 60
+      puts "І йому #{@age} - літня людина"
+    else
+      puts "І йому #{@age} - молодий"
+    end
   end
 end
