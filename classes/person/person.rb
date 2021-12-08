b8b0cb38ead0342bc29edffba81eb3f90987b3b1
tetianarozz <tatianarozputnia@gmail.com>
2021-12-08 13:04:24 +0200

    some fixes

--- classes/person/person.rb
+++ classes/person/person.rb
@@ -6,13 +6,13 @@ class Person
   end
 
   def full_name
-    return (puts "#{@name} #{@second_name}") if @age > 60
+    return(puts "#{@name} #{@second_name}") if @age > 60
 
     puts "#{@name}"
   end
 
   def age_check
-    return (puts "І йому #{@age} - літня людина") if @age > 60
+    return(puts "І йому #{@age} - літня людина") if @age > 60
 
     puts "І йому #{@age} - молодий"
   end
